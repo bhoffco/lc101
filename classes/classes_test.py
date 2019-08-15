9098b63747b6f10e514ba6c613cd2b3b462d2fa6
Brian Hoffman <bhoffco@gmail.com>
2019-08-15 17:34:23 -0400

    again

---
+++ classes/classes_test.py
@@ -0,0 +1,29 @@
+
+
+class Employee:
+
+  num_of_employees = 0
+  raise_amount = 1.04
+
+  def __init__(self, first, last, pay):
+    self.first = first
+    self.last = last
+    self.pay = pay
+    self.email = first + '.' + last + '@company.com'
+
+    Employee.num_of_employees += 1
+
+  def fullname(self):
+      return '{} {}'.format(self.first, self.last)
+
+  def apply_raise(self):
+    self.pay = int(self.pay * self.raise_amount)
+
+
+  
+
+emp_1 = Employee('test', 'user', 65000)
+emp_2 = Employee('john', 'smith', 50000)
+
+print(emp_1.first)
+
